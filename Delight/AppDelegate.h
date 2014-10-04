214890a5b4661816a9b36749c3111fb95554f31f
Kevin Casey <kacasey@berkeley.edu>
2014-10-03 21:32:06 -0700

    Initial Commit

---
+++ Delight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Delight
+//
+//  Created by Kevin Casey on 10/3/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
